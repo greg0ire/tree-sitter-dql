f7d4e333999c0cafa538b7e88be05715efe84aef
Grégoire Paris <postmaster@greg0ire.fr>
2025-10-26 19:57:33 +0100

    Prepare v1.1.0

--- src/parser.c
+++ src/parser.c
@@ -54381,7 +54381,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_dql(void) {
     .max_reserved_word_set_size = 0,
     .metadata = {
       .major_version = 1,
-      .minor_version = 0,
+      .minor_version = 1,
       .patch_version = 0,
     },
   };
